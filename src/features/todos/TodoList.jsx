798c6db7481d4266941e67d855f262a995e488c3
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-31 18:30:13 +0300

    Content conditional rendering using RTQ Query

--- src/features/todos/TodoList.jsx
+++ src/features/todos/TodoList.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
-
+import { useGetTodosQuery } from "../api/apiSlice";
 const TodoList = () => {
   const [newTodo, setNewTodo] = useState("");
+
+  const {
+    data: todos,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetTodosQuery();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -26,12 +34,23 @@ const TodoList = () => {
         <FontAwesomeIcon icon={faUpload} />
       </button>
     </form>
-    // let content;
   );
+
+  let content;
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } else if (isSuccess) {
+    content = JSON.stringify(todos);
+  } else if (isError) {
+    content = <p>{error}</p>;
+  }
+
   return (
     <main>
       <h1>Todo List</h1>
       {newItemSection}
+      {content}
     </main>
   );
 };
